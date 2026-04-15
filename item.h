87278947da0f4dfc48cc6cecceee3c7cb50d644c
greenea21 <greenea21@uww.edu>
2026-04-15 13:29:37 -0500

    Add "item" header file and item struct

---
+++ item.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+struct item {
+	std::string name;
+	double price = 0;
+	int stock = 0;
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
+#include "item.h"
+
+using std::cout;
+using std::endl;
 
 int main() {
-
+	item apple; //read and parse items from a file for stretch goal 8
+	apple.name = "Apple";
+	apple.price = 0.49;
+	apple.stock = 20;
 
 	return 0;
 }
