bca9bad58f2b48b075ecf0d4d5462b24c1e8641d
greenea21 <greenea21@uww.edu>
2026-05-04 15:43:27 -0500

    Comments updated

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 //STRETCH GOALS COMPLETED
-//9: switch statement instead of if-else chain
-//1: vector instead of array for cart
-//10: safeguard against malicious input
+//1: Vector instead of array for cart
+//2: Remove item from cart
 //6: Sales tax
-//7: Receipt output file
+//7: Receipt output to file
+//9: Switch statement instead of if-else chain
+//10: Safeguard against malicious input
 
 //------------------------------------------------------------------------//
 
 //program starts here with menu.start()
 //menu handles all user input and program flow
+//menu manages cart and store objects
 //store handles inventory and stock
 //cart handles cart contents, totals, checkout
+//probably didn't need a menu class; could've just put it here
 
 #include "menu.h"
 
